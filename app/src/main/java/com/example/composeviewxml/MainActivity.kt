413603246af824cad9bdc12e341ea02a2453f0d6
Diego Tobar <dtobar@veritran.com>
2023-10-25 21:18:18 -0500

    Add view binding

--- app/src/main/java/com/example/composeviewxml/MainActivity.kt
+++ app/src/main/java/com/example/composeviewxml/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.composeviewxml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material3.MaterialTheme
+import com.example.composeviewxml.compose.SampleButton
+import com.example.composeviewxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
