114fb94bd59e9703b693eb4d6c81c43f9cca4260
Diego Tobar <dtobar@veritran.com>
2023-10-26 13:48:32 -0500

    Add button view dynamically

--- app/src/main/java/com/example/composeviewxml/SecondActivity.kt
+++ app/src/main/java/com/example/composeviewxml/SecondActivity.kt
@@ -1,8 +1,11 @@
 package com.example.composeviewxml
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.composeviewxml.compose.SampleButton
 import com.example.composeviewxml.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
@@ -13,5 +16,17 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        addView(createViewButton())
+    }
+
+    private fun createViewButton(): Button {
+        val button = Button(this)
+        button.text = "BUTTON VIEW"
+        return  button
+    }
+
+    private fun addView(view: View) {
+        val mainLayout = binding.mainLayout
+        mainLayout.addView(view)
     }
 }
