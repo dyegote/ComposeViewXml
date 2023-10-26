a8ce2242e73b601181c7120567839b7cc1e7c98e
Diego Tobar <dtobar@veritran.com>
2023-10-26 13:53:31 -0500

    Add compose button to layout view dynamically

--- app/src/main/java/com/example/composeviewxml/SecondActivity.kt
+++ app/src/main/java/com/example/composeviewxml/SecondActivity.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.widget.Button
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.platform.ComposeView
 import com.example.composeviewxml.compose.SampleButton
 import com.example.composeviewxml.databinding.ActivitySecondBinding
 
@@ -17,12 +19,26 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
         addView(createViewButton())
+        addView(createComposeButton())
     }
 
     private fun createViewButton(): Button {
         val button = Button(this)
         button.text = "BUTTON VIEW"
         return  button
+    }
+
+
+    private fun createComposeButton(): ComposeView {
+        val composeView = ComposeView(this)
+        composeView.apply {
+            setContent {
+                MaterialTheme {
+                    SampleButton(onCLick = { })
+                }
+            }
+        }
+        return  composeView
     }
 
     private fun addView(view: View) {
